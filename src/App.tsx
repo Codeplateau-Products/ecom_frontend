f5948b8e17d39d49f770a29861d464ce2d436957
queenogbu <queenogbu111@gmail.com>
2023-07-01 11:50:16 +0100

    Added contact route

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,10 @@ function App() {
                   {" "}
                   <Outlet />{" "}
                 </>
-              }
-            >
+              }>
               <Route index element={<h1>Home</h1>} />
               <Route path="about" element={<h1>About</h1>} />
+              <Route path="contact" element={<h1>Contact</h1>} />
               <Route path="register" element={<h1>Register</h1>} />
               <Route path="login" element={<h1>Login</h1>} />
 
@@ -29,7 +29,7 @@ function App() {
                   </ProtectedRoute>
                 }
               />
-                <Route path="*" element={<h1>404</h1>} />
+              <Route path="*" element={<h1>404</h1>} />
             </Route>
           </Routes>
         </BrowserRouter>
